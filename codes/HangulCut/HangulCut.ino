70cffb3d45f1a8391a297f7d12a17046ebdd7170
suyasuyazzang <suyasuyazzang@gmail.com>
2020-11-06 13:23:51 +0900

    한글 출력 분리, 영어/특문도 출력되도록 변경

--- codes/HangulCut/HangulCut.ino
+++ codes/HangulCut/HangulCut.ino
@@ -3,11 +3,9 @@
 TFT_eSPI tft = TFT_eSPI();
 
 #include "hangul.h"
-#include "fontSet.h"
 
-unsigned char cho[] =  {0,0,0,0,0,0,0,0,0,1,3,3,3,1,2,4,4,4,2,1,3,0};
-unsigned char cho2[] = {0,5,5,5,5,5,5,5,5,6,7,7,7,6,6,7,7,7,6,6,7,5};
-unsigned char jong[] = {0,0,2,0,2,1,2,1,2,3,0,2,1,3,3,1,2,1,3,3,1,1};
+String testStr = String("테스트 문자열\n");
+
 void setup() {
   Serial.begin(115200);
   Serial.println();
@@ -19,44 +17,11 @@ void setup() {
 void loop() {
   if (Serial.available()) {
     String inStr = Serial.readString();
-    
-    char inChar[128];
-    inStr.toCharArray(inChar, inStr.length());
 
-    int uniStr[128] = {0};
-    int uniStrLen = utf8toUni(inChar, inStr.length(), uniStr);
-        
+
     tft.fillScreen(TFT_BLACK);
 
-    for (int i = 0; i < uniStrLen; i++) {
-      int curChar = uniStr[i];
-      Serial.println(curChar);
-      if(isHangul(curChar)){
-        int d[3];
-        cutHangul(curChar, d);
-        Serial.print("초성: "); Serial.println(d[0]++);
-        Serial.print("중성: "); Serial.println(d[1]++);
-        Serial.print("종성: "); Serial.println(d[2]);
-
-        int b[3] = {0, 0, 0};
-
-        if(d[2] == 0){
-          b[0] = cho[d[1]];
-          if(d[0] == 1 || d[0] == 24) b[1] = 0;
-          else b[1] = 1;
-        }else{
-          b[0] = cho2[d[1]];
-          if(d[0] == 1 || d[0] == 24) b[1] = 2;
-          else b[1] = 3;
-          b[2] = jong[d[1]];
-        }
-        
-        tft.drawXBitmap(10 + i * 17, 10, glyphs[0][b[0]][d[0]], 16, 16, TFT_WHITE);
-        tft.drawXBitmap(10 + i * 17, 10, glyphs[1][b[1]][d[1]], 16, 16, TFT_WHITE);
-        tft.drawXBitmap(10 + i * 17, 10, glyphs[2][b[2]][d[2]], 16, 16, TFT_WHITE);
-      }else{
-        Serial.println("한글이 아닙니다.");
-      }
-    }
+    printHangul(&inStr, 10, 40, TFT_WHITE, &tft);
+    printHangul(&testStr, 10, 80, TFT_BLUE, &tft);
   }
 }
--- codes/HangulCut/hangul.h
+++ codes/HangulCut/hangul.h
@@ -1,3 +1,10 @@
+#include <TFT_eSPI.h>
+#include "fontSet.h"
+
+unsigned char cho[] =  {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 3, 3, 3, 1, 2, 4, 4, 4, 2, 1, 3, 0};
+unsigned char cho2[] = {0, 5, 5, 5, 5, 5, 5, 5, 5, 6, 7, 7, 7, 6, 6, 7, 7, 7, 6, 6, 7, 5};
+unsigned char jong[] = {0, 0, 2, 0, 2, 1, 2, 1, 2, 3, 0, 2, 1, 3, 3, 1, 2, 1, 3, 3, 1, 1};
+
 int utf8toUni(const char* inStr, int length, int* uniStr) {
   int cur = 0;
 
@@ -12,9 +19,6 @@ int utf8toUni(const char* inStr, int length, int* uniStr) {
     } else if ((inStr[i] & 0xF0) == 0xE0) { //맨 앞 바이트가 1110이면
       uniStr[cur] = (int)(((int)(inStr[i] & 0x0F)) << 12 | (inStr[i + 1] & 0x3F) << 6 | (inStr[i + 2] & 0x3F));
       i += 2; //두 바이트 썼으니까...
-
-    } else if ((inStr[i] & 0xF8) == 0xF0){
-      uniStr[cur] = 0xFFFF; //이 범위 문자가 들어오면 처리 못하니까 일단 임시로...
     } else {
       uniStr[cur] = 0xFFFF; //버그!!!!!!!
     }
@@ -24,7 +28,7 @@ int utf8toUni(const char* inStr, int length, int* uniStr) {
   return cur--;
 }
 
-int isHangul(int uniChar){
+int isHangul(int uniChar) {
   return 0xAC00 <= uniChar && uniChar <= 0xD7A3;
 }
 
@@ -32,12 +36,60 @@ int cutHangul(int uniChar, int * d) {
   if (isHangul(uniChar)) { //한글 범위 내의 글자라면
     int c = uniChar - 0xAC00; //일단 0xAC00을 빼고...
 
-    d[0] = c / (21 * 28); //초성
-    d[1] = (c - (d[0] * 21 * 28)) / 28; //중성
-    d[2] = c - (d[0] * 21 * 28) - d[1] * 28; //종성
+    d[0] = (c / (21 * 28)); //초성
+    d[1] = ((c - (d[0] * 21 * 28)) / 28); //중성
+    d[2] = (c - (d[0] * 21 * 28) - d[1] * 28); //종성
+
+    d[0]++;
+    d[1]++;
 
     return 0;
-  }else{ //한글 범위가 아니면 -1을 반환
+  } else { //한글 범위가 아니면 -1을 반환
     return -1;
+  }
+}
+
+int printHangulChar(int* d, int x, int y, uint16_t color, TFT_eSPI* tft) {
+
+  int b[3] = {0, 0, 0};
+
+  if (d[2] == 0) {
+    b[0] = cho[d[1]];
+    if (d[0] == 1 || d[0] == 24) b[1] = 0;
+    else b[1] = 1;
+  } else {
+    b[0] = cho2[d[1]];
+    if (d[0] == 1 || d[0] == 24) b[1] = 2;
+    else b[1] = 3;
+    b[2] = jong[d[1]];
+  }
+
+  tft->drawXBitmap(x, y, glyphs[0][b[0]][d[0]], 16, 16, color);
+  tft->drawXBitmap(x, y, glyphs[1][b[1]][d[1]], 16, 16, color);
+  tft->drawXBitmap(x, y, glyphs[2][b[2]][d[2]], 16, 16, color);
+}
+
+int printHangul(String* inStr, int x, int y, uint16_t color, TFT_eSPI* tft) {
+
+  char tempChar[128];
+  inStr->toCharArray(tempChar, inStr->length());
+
+  int uniStr[128] = {0};
+  int uniStrLen = utf8toUni(tempChar, inStr->length(), uniStr);
+
+  int curx = x;
+
+  for (int i = 0; i < uniStrLen; i++) {
+    int curChar = uniStr[i];
+    if (isHangul(curChar)) {
+      int d[3];
+      cutHangul(curChar, d);
+      printHangulChar(d, curx, y, color, tft);
+      curx += 17;
+    } else {
+      tft->setTextColor(color, TFT_BLACK);
+      tft->drawChar(curChar, curx, y, 2);
+      curx += 8;
+    }
   }
 }
