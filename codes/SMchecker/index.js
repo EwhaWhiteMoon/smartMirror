23d3929042c4896ed039d46ad8b9c589c4c64a75
suyasuyazzang <suyasuyazzang@gmail.com>
2020-11-12 22:54:38 +0900

    일단 완성...

--- codes/SMchecker/index.js
+++ codes/SMchecker/index.js
@@ -1,22 +1,28 @@
 
 const axios = require('axios')
 const cheerio = require('cheerio')
+const fs = require('fs')
 
 const query = encodeURI("배방고등학교 급식")
 
 axios.get(`https://search.naver.com/search.naver?query=${query}`).then(r =>{
     const $ = cheerio.load(r.data)
     let menu = []
-    menuList = $('div.school_menu').children().children('li.menu_info').each((i,elem)=>{
+
+    $('div.school_menu').children().children('li.menu_info').each((i,elem)=>{
         menu[i] = {}
-        menu[i].date = $(elem).children('strong').text().split('[')[0]
+        menu[i].date = $(elem).children('strong').text().split('[')[0].replace('월 ','-').replace('일 ','').replace(/-([1-9])$/,'-0$1')
         menu[i].list = $(elem).children('ul').children('li').map((i, elem)=>{
             return $(elem).text().replace(/\**[0-9.]+/i,'')
         }).get()
     })
+    today = new Date().toISOString().substr(5, 5);
+    console.log(menu)
 
-    console.log(menu.reduce((acc, cur, idx, arr)=> `${acc}\n${cur.date}\n${cur.list.join('\n')}\n`, ''))
-    
+    todaySM = menu.find(e => e.date == today)
+    console.log(todaySM)
+
+    fs.writeFileSync('todaySM.txt',todaySM.list.join('\n'))
 }).catch(e => {
     console.log(e)
 })
